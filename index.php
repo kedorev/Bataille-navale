250c7112e1593664dea61fbd9320954e86e02fbf
kedorev <kedorev@gmail.com>
2017-02-27 11:19:07 +0100

    First push with jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kedorev
+ * Date: 27/02/17
+ * Time: 11:13
+ */
+
+?>
+
+<html>
+    <head>
+        <title>Bataille navalle</title>
+        <script type="application/javascript" src="JS/jquery-3.1.1.min.js"></script>
+    </head>
+</html>
